2b2300d90480c93094783b600cb66d18742ce2e6
Isuka <kyumin1404@gmail.com>
2023-06-03 19:28:30 +0900

    Practice basic 0603

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,59 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-            title: Text('안녕')
+          centerTitle: false,
+          leading: TextButton(onPressed: null, child : Text('금호동3가',
+            style: TextStyle(color: Colors.black, fontSize: 18,letterSpacing: 1.2,fontWeight: FontWeight.w700)),
+           ),leadingWidth: 100,
+          actions: [IconButton(onPressed: null, icon: Icon(Icons.search)),IconButton(onPressed: null, icon: Icon(Icons.menu)),IconButton(onPressed: null, icon: Icon(Icons.notifications))],
+          backgroundColor: Color(0xffffffff),
         ),
         body: Container(
-          width: 50, height: 50, color: Colors.blue,
-            padding: EdgeInsets.all(20) // 안쪽여백
-
+          width: double.infinity, height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset('assets/images/camera.jpg',width: 150,),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('캐논 DSLR 100D(단렌즈, 충전기 16기가SD 포함)'),
+                    Text('금호동 3가'),
+                    Text('7000원'),
+                    Text('Icon')
+                  ],
+                )
+              )
+              ]
+          ),
+        )
+      ),
+      );
+    /*body: SizedBox(
+          child: ElevatedButton(
+            onPressed: (){},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Color(0xffaaaaaa))
+            ),
+            child: Text('글자')
+          ),
+          //alignment: Alignment.topCenter,
+          /*child: Container(
+            width: double.infinity, height: 100,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              border: Border.all(color: Colors.black)
+            ),
+            //padding : EdgeInsets.fromLTRB(0, 30, right, bottom)
+            padding: EdgeInsets.all(20), // 안쪽여백
+              child: Text('ddddd',
+              style: TextStyle(fontWeight: FontWeight.w700, color: Color(
+                  0xffff0000)),),
+          ),*/
         ),
-        /*
+        */
+    /*
         bottomNavigationBar: BottomAppBar(
         child: SizedBox(  // width, height, child만 필요한 박스일경우 사용
           height: 70,
@@ -35,9 +80,6 @@ class MyApp extends StatelessWidget {
             )
           ),
         */
-      ),
-      );
-
       //Image.asset('dog.png') 50lp == 1.2cm
       //Image.asset('경로~~~');
       //Icon(Icons.star)
